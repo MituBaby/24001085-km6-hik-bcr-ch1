abd9cc4df12d1472d1a5bc7b38de924bd6652070
mitu baby <ucihazhaske@gmail.com>
2024-02-24 16:36:48 +0700

    Fix Testimonial sectioin and Clean Up

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
-var btn = document.getElementById("btn");
-
-btn.addEventListener("click", function () {
-  console.log("Tombol di klik");
-  alert("Apakah anda yakin?");
-});
-
 var btnNav = document.getElementById("btn-nav");
 var navbar = document.getElementById("navbar");
 var navigation = document.getElementById("navigation");
 var btnCloseNav = document.getElementById("btn-close-nav");
+var BCR = document.getElementById("BCR");
 var ourService = document.getElementById("OurService");
 var whyUs = document.getElementById("WhyUs");
 var Testi = document.getElementById("Testi");
@@ -27,6 +21,12 @@ btnCloseNav.addEventListener("click", function () {
   bgNav.remove();
 });
 
+BCR.addEventListener("click", function () {
+  navbar.classList.remove("active");
+  btnCloseNav.classList.remove("active");
+  var bgNav = document.getElementById("bg-nav");
+  bgNav.remove();
+});
 ourService.addEventListener("click", function () {
   navbar.classList.remove("active");
   btnCloseNav.classList.remove("active");
@@ -54,6 +54,41 @@ faq.addEventListener("click", function () {
 
 btnNav.addEventListener("click", function () {
   var newBg = document.createElement("div");
+
   newBg.id = "bg-nav";
+
   navigation.appendChild(newBg);
 });
+
+// start testi
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+// end testi
